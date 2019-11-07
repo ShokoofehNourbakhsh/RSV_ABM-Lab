7e3cc2a245ace2fdfa43334a1814f9771672cb46
ShokoofehNourbakhsh <sho.nourbakhsh@gmail.com>
2019-11-07 15:34:05 -0500

    revised_2

--- RSV.jl
+++ RSV.jl
@@ -1,7 +1,7 @@
 ## single agent-based model for Respiratory syncytial virus (RSV)
 ## Developed by Shokoofeh & Affan
 
-module rsv
+module RSV
 
 ## Loading necessary packages
 using DataFrames
@@ -15,7 +15,7 @@ mutable struct Human
     idx::Int64
     health::Int64       # 0 = susc, 1 = infected
     age::Int64          # in yeras
-    age_group::Int64    # G1 = < 1, G2= 2, G3= 3, G4= 4, G5= 5:19, G6= > 19
+    agegroup::Int64    # G1 = < 1, G2= 2, G3= 3, G4= 4, G5= 5:19, G6= > 19
     smoker::Int64       # 1 = no,  2 = yes
     preterm:: Int64     # 1 = no,  2 = yes
     Human() = new()
@@ -30,15 +30,14 @@ end
 ######## global variables
 # Nunavik age population. Downloaded from Census Canada 2016
 # data organized in file 'parameters_Shokoofeh'
-const agedist =  Categorical(@SVector [0.022727273, 0.026136364, 0.026136364, 0.026136364, 0.022727273, 0.310606061, 0.565530303]) # categorical probability distribution of discrete age_groups
-const agebraks = @SVector [0:0.99, 1:1.99, 2:2.99, 3:3.99, 4:4.99, 5:18.99, 19:100] age_groups
+const agedist =  Categorical(@SVector[0.022727273, 0.026136364, 0.026136364, 0.026136364, 0.022727273, 0.310606061, 0.565530303]) # categorical probability distribution of discrete age_groups
+const agebraks = @SVector[0:0.99, 1:1.99, 2:2.99, 3:3.99, 4:4.99, 5:18.99, 19:100] #age_groups
 const predist = Categorical(@SVector[0.972027972, 0.027972028])  # distribution of preterm infants
 const smokdist = Categorical(@SVector[0.37, 0.63])  # distribution of preterm infants
 const dwelsize_dist = Categorical(@SVector[0.208275862,0.164137931,0.153103448,0.16137931,0.313103448]) # household's size: 1, 2, 3, 4, 5 >
-const P = ModelParameters() # I'll make the parameter file
+#const P = ModelParameters() # I'll make the parameter file
 const humans = Array{Human}(undef, 13200) # 13200 is total population of Nunavik
 const dwells = Array{Dwelling}(undef, 1000)
-
 
 
 export humans, dwells
@@ -51,7 +50,7 @@ function init_population()
         humans[i].idx = i
         humans[i].health = 0
         humans[i].age = 0
-        humans[i].age_group = 0
+        humans[i].agegroup = 0
         humans[i].smoker = 1
         humans[i].preterm = 1
     end
@@ -70,14 +69,14 @@ export init_dwellings
 
 ## randomly assigns age, age_group, preterm < 1 and smoker 19+
 function apply_charectristics(x::Human)
-    x.age_group = rand(agedist)
-    x.age = rand(agebraks[x.group])
+    x.agegroup = rand(agedist)
+    x.age = rand(agebraks[x.agegroup])
 
-    if x.age_group == 1   #assign preterm if < 1 years old
+    if x.agegroup == 1   #assign preterm if < 1 years old
         x.preterm = rand(predist)
     end
 
-    if x.age_group == 7  #assign smoker if 19+ years old
+    if x.agegroup == 7  #assign smoker if 19+ years old
         x.smoker = rand(smokdist)
     end
 end
