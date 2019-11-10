9526e42c2cf91d5a242fcba3c969817f16685271
ShokoofehNourbakhsh <sho.nourbakhsh@gmail.com>
2019-11-10 09:41:27 -0500

    added global variables, changed init_dwellings(), builded init_population() and apply_humandistribution()

--- RSV.jl
+++ RSV.jl
@@ -24,6 +24,7 @@ end
 mutable struct Dwelling
     idx::Int64
     humans::Array{Int64}
+    size::Int64
     Dwelling() = new()
 end
 
@@ -34,7 +35,8 @@ const agedist =  Categorical(@SVector[0.022727273, 0.026136364, 0.026136364, 0.0
 const agebraks = @SVector[0:0.99, 1:1.99, 2:2.99, 3:3.99, 4:4.99, 5:18.99, 19:100] #age_groups
 const predist = Categorical(@SVector[0.972027972, 0.027972028])  # distribution of preterm infants
 const smokdist = Categorical(@SVector[0.37, 0.63])  # distribution of preterm infants
-const dwelsize_dist = Categorical(@SVector[0.208275862,0.164137931,0.153103448,0.16137931,0.313103448]) # household's size: 1, 2, 3, 4, 5 >
+const dwellsize_dist = Categorical(@SVector[0.208275862,0.164137931,0.153103448,0.16137931,0.313103448]) # household's size dist.: 1, 2, 3, 4, 5 >
+const dwellbraks = @SVector[1:1, 2:2, 3:3, 4:4, 5:15] # house size
 #const P = ModelParameters() # I'll make the parameter file
 const humans = Array{Human}(undef, 13200) # 13200 is total population of Nunavik
 const dwells = Array{Dwelling}(undef, 1000)
@@ -44,7 +46,7 @@ export humans, dwells
 
 
 ############################ setting up agents for model
-function init_population()
+function init_humans()
     @inbounds for i = 1:length(humans)
         humans[i] = Human()              ## create an empty human
         humans[i].idx = i
@@ -55,14 +57,14 @@ function init_population()
         humans[i].preterm = 1
     end
 end
-export init_population
+export init_humans
 
 function init_dwellings()
     @inbounds for i = 1:length(dwells)
         dwells[i] = Dwelling()             ## create an empty house
         dwells[i].idx = i
-        dwells[i].humans = [1,2,3]
-        dwells[i].size = 0
+        dwells[i].size = rand(dwellbraks[rand(dwellsize_dist)])
+        dwells[i].humans = zeros(SVector{dwells[i].size,Int64})
     end
 end
 export init_dwellings
@@ -82,17 +84,34 @@ function apply_charectristics(x::Human)
 end
 export apply_charectristics
 
-## randomly assigns household's size to privet dwellings
-function apply_dwelsize(x::Dwelling)
-    x.size = rand(dwelsize_dist)
+###### Assign charectristics to humans: initial populations
+function init_population()
+    @inbounds for i = 1:length(humans)
+        apply_charectristics(humans[i]) #charectristics; age,agegroup,preterm,smoker
+    end
 end
-export apply_dwelsize
+export init_population
 
-## randomly distributes humans to privet dwellings
-function apply_humandistribution(x::Dwelling)
-    x.human = rand(dwelsize_dist)
+######## randomly distributes humans to privet dwellings
+function apply_humandistribution(x::Human, y::Dwelling)
+    humans_G7 = findall(x -> x.agegroup == 7, humans) #find 19+ yearsold
+    humans_G6 = findall(x -> x.agegroup == 6, humans) #find 5 > 19 yearsold
+    humans_G5 = findall(x -> x.agegroup == 5, humans) #find 4:5 yearsold
+    humans_G4 = findall(x -> x.agegroup == 4, humans) #find 3:4 yearsold
+    humans_G3 = findall(x -> x.agegroup == 3, humans) #find 2:3 yearsold
+    humans_G2 = findall(x -> x.agegroup == 2, humans) #find 1:2 yearsold
+    humans_G1 = findall(x -> x.agegroup == 1, humans) #find < 1 yerasold
+
+     for i in humans_G7
+        #eligible dwells are not occuppied fully and at least has one free space for humans
+        eligible_dwells = findall(y -> length(y.humans[y.humans .== 0]) > 0, dwells)
+        id = rand(eligible_dwells)
+        idd = findfirst(z -> z == 0, dwells[id].humans)
+        dwells[id].humans[idd] = humans_G7[i]
+    end
 end
 export apply_humandistribution
+
 
 
 end # module
